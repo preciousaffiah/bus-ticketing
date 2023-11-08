bc850ee183577d3f61b514faa75dc25c06c17e95
preciousaffiah <preciousaffiah205@gmail.com>
2023-11-09 00:32:31 +0100

    bug:db

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -19,13 +28,11 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
-try {
-    db_1.default.authenticate(),
-        db_1.default.sync({ force: false }),
-        console.log("Connection has been established successfully.");
-}
-catch (error) {
-    console.error("Unable to connect to the database:", error);
-}
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    yield db_1.default.authenticate();
+    yield db_1.default.sync({ force: false })
+        .then((r) => console.log("Connection has been established successfully."))
+        .catch((e) => console.error("Unable to connect to the database:", e));
+}))();
 exports.default = app;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -20,12 +20,11 @@ app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
-try {
-  sequelize.authenticate(),
-    sequelize.sync({ force: false }),
-    console.log("Connection has been established successfully.");
-} catch (error) {
-  console.error("Unable to connect to the database:", error);
-}
+(async () => {
+  await sequelize.authenticate()
+  await sequelize.sync({ force: false })
+    .then((r) => console.log("Connection has been established successfully."))
+    .catch((e) => console.error("Unable to connect to the database:", e));
+})();
 
 export default app;
